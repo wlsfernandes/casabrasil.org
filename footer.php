794849b9e29dc60dfbca6fec15e63b3be457a875
Junior <wlsfernandes@gmail.com>
2024-04-15 18:19:00 -0400

    casa brasil

---
+++ footer.php
@@ -0,0 +1,75 @@
+<!-- SUBSCRIBE -->
+<div class="highlight-bg">
+  <div class="container">
+    <div class="row">
+      <form action="subscribe.php" method="post" class="form subscribe-form" role="form" id="subscribeForm">
+        <div class="form-group col-md-3 hidden-sm">
+          <h5 class="susbcribe-head"> SUBSCRIBE <span>TO OUR NEWSLETTER</span></h5>
+        </div>
+        <div class="form-group col-sm-8 col-md-6">
+          <label class="sr-only">Email address</label>
+          <input type="email" class="form-control input-lg" placeholder="Enter email" name="email" id="address" data-validate="validate(required, email)" required>
+          <span class="help-block" id="result"></span> </div>
+        <div class="form-group col-sm-4 col-md-3">
+          <button type="submit" class="btn btn-lg btn-primary btn-block">Subscribe Now →</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+<!-- END SUBSCRIBE --> 
+
+<!-- FOOTER -->
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6 col-md-3">
+        <h5>ABOUT THE CHURCH</h5>
+        <p>For the word of God is living and active. Sharper than any double-edged sword, it penetrates even to dividing soul and spirit, joints and marrow; it judges the thoughts and attitudes.</p>
+      </div>
+      <div class="col-sm-6 col-md-3">
+        <h5>QUICK LINKS</h5>
+        <ul class="footer-links">
+          <li><a href="#">Upcoming events</a></li>
+          <li><a href="#">Ministries</a></li>
+          <li><a href="#">Recent Sermons</a></li>
+          <li><a href="#">Contact us</a></li>
+        </ul>
+      </div>
+      <div class="col-sm-6 col-md-3">
+        <h5>OUR ADDRESS</h5>
+        <p> Catholic Church<br>
+          121 King Street, Melbourne <br>
+          Victoria 3000 Australia<br>
+          <br>
+          Phone: +61 3 8376 6284<br>
+          Email: <a href="#">mail@catholicwebsite.com</a></p>
+      </div>
+      <div class="col-sm-6 col-md-3">
+        <h5>CONNECT</h5>
+        <div class="social-icons"><a href="#"><img src="images/fb-icon.png" alt="social"></a> <a href="#"><img src="images/tw-icon.png" alt="social"></a> <a href="#"><img src="images/in-icon.png" alt="social"></a></div>
+      </div>
+    </div>
+  </div>
+  <div class="copyright">
+    <div class="container">
+      <p class="text-center">Copyright © 2014 All rights reserved</p>
+    </div>
+  </div>
+</footer>
+
+
+<!-- // END --> 
+
+<!-- Bootstrap core JavaScript
+================================================== --> 
+<!-- Placed at the end of the document so the pages load faster --> 
+<script src="js/jquery.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/owl.carousel.min.js"></script> 
+<script src="js/ketchup.all.js"></script> 
+<script src="js/fancybox.js"></script> 
+<script src="js/script.js"></script> 
+
+
+<!-- // END FOOTER --> 
